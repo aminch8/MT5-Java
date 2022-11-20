7377226214390a7e16f0084438585496fc3adde3
Amin Chokan <amin.chokan@gmail.com>
2022-11-20 22:02:21 +0330

    DEV: Gitignore

--- src/main/java/com/mt5/core/Main.java
+++ src/main/java/com/mt5/core/Main.java
@@ -1,8 +1,6 @@
 package com.mt5.core;
 
-import com.mt5.core.enums.TimeFrame;
-import com.mt5.core.interfaces.OnCandleUpdate;
-import com.mt5.core.interfaces.OnTickUpdate;
+
 import com.mt5.core.livedata.MT5LiveData;
 import com.mt5.core.services.MT5Client;
 
@@ -13,6 +11,6 @@ public class Main {
         MT5Client mt5Client = new MT5Client.MT5ClientFactory(2201,2202).setHost("localhost").build();
         MT5LiveData mt5LiveData = new MT5LiveData.MT5LiveDataFactory(2203,mt5Client).build();
 
-        
+
     }
 }
