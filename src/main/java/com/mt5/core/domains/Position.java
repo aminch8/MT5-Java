eb29c0e8a1540056a929f2a999f548c43420ee33
Amin Chokan <amin.chokan@gmail.com>
2023-11-09 15:49:50 +0330

    DEV: Constructor position

--- src/main/java/com/mt5/core/domains/Position.java
+++ src/main/java/com/mt5/core/domains/Position.java
@@ -1,6 +1,7 @@
 package com.mt5.core.domains;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mt5.core.domains.requests.rest.PositionRest;
 import com.mt5.core.enums.PositionType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,4 +24,16 @@ public class Position {
     @JsonProperty("takeprofit")
     private Number takeProfit;
     private Number volume;
+
+    public Position(PositionRest positionRest) {
+        this.id = positionRest.getId();
+        this.magic = Long.parseLong(positionRest.getMagic().toString());
+        this.symbol = positionRest.getSymbol();
+        this.type = positionRest.getPositionType();
+        this.timeSetup = positionRest.getTimeSetup().getTime()/1000;
+        this.openPrice = positionRest.getOpen();
+        this.stopLoss = positionRest.getStoploss();
+        this.takeProfit = positionRest.getTakeprofit();
+        this.volume = positionRest.getVolume();
+    }
 }
